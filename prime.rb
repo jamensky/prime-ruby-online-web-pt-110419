dbab956847079dfd0003d0bb3bdffaa7788b2529
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-19 02:07:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,16 @@
 
 def prime?(n)
+<<<<<<< HEAD
   if n < 2
     return false
   else (2..n - 1).any?{|x| n % x == 0}
   end
 end
+=======
+  if (2..n).any?{|x| x % n == 0}
+    return true 
+  else 
+    return false 
+  end 
+end 
+>>>>>>> 69b8da95b455929f9fd80e45b971b2017ad9bfdb
